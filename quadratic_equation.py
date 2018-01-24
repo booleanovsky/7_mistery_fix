e73642227eb6798c3f67ddd532b8cf4b01f0fa5c
Gadzhi <35730082+booleanovsky@users.noreply.github.com>
2018-01-24 17:37:44 +0300

    пофиксил

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -6,6 +6,8 @@ def get_roots(a, b, c):
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
-        return root1, None
-    else:
         return root1, root2
+    elif discriminant !=0:
+		return root1, None
+    else:
+        return None 
